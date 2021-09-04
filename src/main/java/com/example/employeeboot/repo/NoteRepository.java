28179876f13c1625652ec8dc041b3eadcd1e0d2d
Narendra <nkadiri09@gmail.com>
2021-09-04 10:46:52 -0400

    first commit

---
+++ src/main/java/com/example/employeeboot/repo/NoteRepository.java
@@ -0,0 +1,10 @@
+package com.example.employeeboot.repo;
+
+import com.example.employeeboot.entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
